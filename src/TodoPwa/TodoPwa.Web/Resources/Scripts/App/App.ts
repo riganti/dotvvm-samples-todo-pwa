190d4c070bdbe1c1ae34c49b0af986be7e6a41c5
Roman Jašek <roman.jasek@riganti.cz>
2020-04-19 18:06:30 +0200

    Added Android application using TWA. Added assetlinks.json. Added checking for offline status without periodic checking.

--- src/TodoPwa/TodoPwa.Web/Resources/Scripts/App/App.ts
+++ src/TodoPwa/TodoPwa.Web/Resources/Scripts/App/App.ts
@@ -2,18 +2,29 @@
 const dotvvm = (window as any).dotvvm as DotVVM;
 
 dotvvm.events.init.subscribe(async () => {
-    setInterval(_ => {
+    // Periodically checking for online status
+    setInterval(() => {
         const request = new XMLHttpRequest();
         request.open("GET", "/online-status-check");
         request.timeout = 3000;
-        request.onload = _ => {
+        request.onload = () => {
             if (request.status === 200) {
                 dotvvm.viewModels.root.viewModel.IsPageOffline(false);
             }
         };
-        request.ontimeout = _ => {
+        request.ontimeout = () => {
             dotvvm.viewModels.root.viewModel.IsPageOffline(true);
         };
+
         request.send();
     }, 10000);
+
+    // Listening to online status changes
+    window.addEventListener("online", () => {
+        dotvvm.viewModels.root.viewModel.IsPageOffline(false);
+    });
+
+    window.addEventListener("offline", () => {
+        dotvvm.viewModels.root.viewModel.IsPageOffline(true);
+    });
 });
